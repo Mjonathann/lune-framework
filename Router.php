250eb7e48a55af237f08303b9a126d1a605f0670
Marvin <m.jonathan.iraheta@gmail.com>
2024-02-26 20:32:00 -0600

    Basic router

---
+++ Router.php
@@ -0,0 +1,45 @@
+<?php 
+ require_once "./HttpMethod.php";
+ require_once "./HttpNotFoundException.php";
+
+ class Router{
+   
+    protected array $routes = [];
+
+    public function __construct(){
+        foreach (HttpMethod::cases() as $method) {
+            $this->routes[$method->value] = [];
+        }
+    }
+
+    public function get(string $uri, callable $action){
+        $this->routes[HttpMethod::GET->value][$uri] = $action;
+    }
+
+    public function post(string $uri, callable $action){
+        $this->routes[HttpMethod::POST->value][$uri] = $action;
+    }
+
+    public function put(string $uri, callable $action){
+        $this->routes[HttpMethod::PUT->value][$uri] = $action;
+    }
+    public function patch(string $uri, callable $action){
+        $this->routes[HttpMethod::PATCH->value][$uri] = $action;
+    }
+    public function delete(string $uri, callable $action){
+        $this->routes[HttpMethod::DELETE->value][$uri] = $action;
+    }
+
+    public function resolve() {
+        $method = $_SERVER['REQUEST_METHOD'];
+        $uri = $_SERVER['REQUEST_URI'];
+
+        $action = $this->routes[$method][$uri] ?? null;
+
+        if (is_null($action)) {
+            throw new HttpnotFoundException();
+        }
+
+        return $action;
+    }
+ }
